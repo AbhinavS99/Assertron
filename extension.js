49145702e24d45fbe28db04b367c5ad9d9800179
Abhinav Sharma <abhinav9921@outlook.com>
2023-04-26 15:31:05 +0530

    added webview

--- extension.js
+++ extension.js
@@ -1,33 +1,241 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const axios = require('axios');
+const path = require('path');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	let editor;
+	let selectedRange;
+	let selectedText;
+	let responseData;
+	let responseView;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "assertron" is now active!');
+	function closeWebView(){
+		responseView.dispose();
+	}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('assertron.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	function replaceCode(){
+		editor.edit((editBuilder) => {
+			editBuilder.replace(selectedRange, responseData);
+		});
+	}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Assertron!');
-	});
+	function getDiffLines(oldCode, newCode){
+		const oldLines = oldCode.split('\n');
+		const newLines = newCode.split('\n');
+		let inds = [];
+		for (let i=0; i<newLines.length; i++){
+			const newLine = newLines[i];
+			if (oldLines.includes(newLine)){
+				inds.push(i);
+				console.log(newLine);
+			}
+		}
+		return inds;
+	}
+	
+	async function generateNaiveAssertion() {
+		editor = vscode.window.activeTextEditor;
+		if (!editor){
+			vscode.window.showErrorMessage('No active editor found');
+      		return;
+		}
 
+		const selection = editor.selection;
+		selectedText = editor.document.getText(selection);
+		if (!selectedText) {
+			vscode.window.showErrorMessage('No text selected');
+			return;
+		}
+
+		selectedRange = new vscode.Range(selection.start, selection.end);
+		const originalFileUri = editor.document.uri;
+    	const originalFileExt = path.extname(originalFileUri.fsPath);
+		
+		vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			title: 'Generating Assertions...',
+			cancellable: false
+		  }, () => {
+			return new Promise(resolve => {
+				// @ts-ignore
+				axios.post('http://127.0.0.1:5000/genNaiveAssert', { code: selectedText, code_extension: originalFileExt})
+				.then(response => {
+					resolve();
+					responseData = response.data;
+					const diffInds = getDiffLines(selectedText, responseData);
+					console.log(diffInds);
+					const viewType = 'responseView';
+					const viewTitle = 'Generated Assertions';
+					responseView = vscode.window.createWebviewPanel(viewType, viewTitle, vscode.ViewColumn.Two, {enableScripts: true});
+					responseView.webview.onDidReceiveMessage(message => {
+						if (message.command === 'discard') {
+
+						} else if (message.command === 'merge') {
+
+						}
+						else if (message.command === 'close') {
+							closeWebView();
+						}
+					});
+					
+					const vscodeStylesheetPath = vscode.Uri.file(
+						'/Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/workbench.desktop.main.css'
+					).with({ scheme: 'vscode-resource' });
+					responseView.webview.html = `
+					<!DOCTYPE html>
+						<head>
+							<link href="${vscodeStylesheetPath}" rel="stylesheet">
+							<style>
+								body {
+									background-color: var(--vscode-editor-background);
+									color: var(--vscode-editor-foreground);
+									font-family: var(--vscode-editor-font-family);
+									font-size: var(--vscode-editor-font-size);
+									line-height: var(--vscode-editor-line-height);
+									padding: 20px;
+									height: 100%;
+									overflow: hidden;
+								}
+								h1 {
+									margin-top: 0;
+								}
+							</style>		
+						</head>
+						<body>
+							<div>
+								<div>
+									<h1><pre>${responseData}</pre></h1>
+								</div>
+							</div>
+							<script>
+								const vscode = acquireVsCodeApi();
+								const closeButton = document.getElementById('closeBtn');
+								closeButton.addEventListener('click', () => {
+									vscode.postMessage({
+										command: 'close'
+									});
+								});
+							</script>
+						</body>
+					</html>
+				`;
+				})
+				.catch(error => {
+					console.error(error);
+					resolve();
+					vscode.window.showErrorMessage('Some unexpected error occurred.');
+				});
+			})
+		})
+
+	}
+
+	async function generateFewShotAssertion() {
+		editor = vscode.window.activeTextEditor;
+		if (!editor){
+			vscode.window.showErrorMessage('No active editor found');
+      		return;
+		}
+
+		const selection = editor.selection;
+		selectedText = editor.document.getText(selection);
+		if (!selectedText) {
+			vscode.window.showErrorMessage('No text selected');
+			return;
+		}
+
+		selectedRange = new vscode.Range(selection.start, selection.end);
+		const originalFileUri = editor.document.uri;
+    	const originalFileExt = path.extname(originalFileUri.fsPath);
+		
+		vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			title: 'Generating Assertions...',
+			cancellable: false
+		  }, () => {
+			return new Promise(resolve => {
+				// @ts-ignore
+				axios.post('http://127.0.0.1:5000/genFewShotAssert', { code: selectedText, code_extension: originalFileExt})
+				.then(response => {
+					resolve();
+					responseData = response.data;
+					const diffInds = getDiffLines(selectedText, responseData);
+					console.log(diffInds);
+					const viewType = 'responseView';
+					const viewTitle = 'Generated Assertions';
+					responseView = vscode.window.createWebviewPanel(viewType, viewTitle, vscode.ViewColumn.Two, {enableScripts: true});
+					responseView.webview.onDidReceiveMessage(message => {
+						if (message.command === 'discard') {
+
+						} else if (message.command === 'merge') {
+
+						}
+						else if (message.command === 'close') {
+							closeWebView();
+						}
+					});
+					
+					const vscodeStylesheetPath = vscode.Uri.file(
+						'/Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/workbench.desktop.main.css'
+					).with({ scheme: 'vscode-resource' });
+					responseView.webview.html = `
+					<!DOCTYPE html>
+						<head>
+							<link href="${vscodeStylesheetPath}" rel="stylesheet">
+							<style>
+								body {
+									background-color: var(--vscode-editor-background);
+									color: var(--vscode-editor-foreground);
+									font-family: var(--vscode-editor-font-family);
+									font-size: var(--vscode-editor-font-size);
+									line-height: var(--vscode-editor-line-height);
+									padding: 20px;
+									height: 100%;
+									overflow: hidden;
+								}
+								h1 {
+									margin-top: 0;
+								}
+							</style>		
+						</head>
+						<body>
+							<div>
+								<div>
+									<h1><pre>${responseData}</pre></h1>
+								</div>
+							</div>
+							<script>
+								const vscode = acquireVsCodeApi();
+								const closeButton = document.getElementById('closeBtn');
+								closeButton.addEventListener('click', () => {
+									vscode.postMessage({
+										command: 'close'
+									});
+								});
+							</script>
+						</body>
+					</html>
+				`;
+				})
+				.catch(error => {
+					console.error(error);
+					resolve();
+					vscode.window.showErrorMessage('Some unexpected error occurred.');
+				});
+			})
+		})
+
+	}
+
+	const disposable = vscode.commands.registerCommand('assertron.generateNaiveAssert', generateNaiveAssertion);
+	const fewShotdisposable = vscode.commands.registerCommand('assertron.generateFewShotAssert', generateFewShotAssertion);
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(fewShotdisposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
